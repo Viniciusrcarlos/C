c191e1ce6ab7aad3a3497b2c0a3a1f2eb4ef1eae
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2022-05-31 22:29:52 -0300

    exerc

--- 001/exercicio01.c
+++ 001/exercicio01.c
@@ -4,8 +4,7 @@ int main(){
     char nome;
 
     printf("Qual o seu nome?\n");
-    scanf("%c%*c",nome);
-
-    printf("O nome é:\n %c", nome);
+    scanf("%c%*c",&nome);
+    printf("O nome e:\n %c", nome);
     return 0;
 }
---
+++ 001/exercicio02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char nome;
+    
+
+    return 0;
+}
